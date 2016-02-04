4173aaa8050e85c3927d6966e604eabafbac03f2
Brent Picasso <brent@autosportlabs.com>
2016-02-04 00:27:16 -0800

    re-added choicePopup

--- autosportlabs/racecapture/views/util/alertview.py
+++ autosportlabs/racecapture/views/util/alertview.py
@@ -84,7 +84,30 @@ def alertPopup(title, msg):
                       content=Label(text = msg),
                       size_hint=(None, None), size=(dp(600), dp(200)))
     popup.open()    
- 
+     
+def choicePopup(title, msg, choice1, choice2, answerCallback):
+    content = ChoicePopup(text=msg, choice1=choice1, choice2=choice2)
+    content.bind(on_answer=answerCallback)
+    popup = Popup(title=title,
+                    content=content,
+                    size_hint=(None, None),
+                    size=(dp(600),dp(200)),
+                    auto_dismiss= False)
+    popup.open()
+    return popup
+
+class ChoicePopup(GridLayout):
+    text = StringProperty()
+    choice1 = StringProperty('Yes')
+    choice2 = StringProperty('No')
+    
+    def __init__(self,**kwargs):
+        self.register_event_type('on_answer')
+        super(ChoicePopup,self).__init__(**kwargs)
+        
+    def on_answer(self, *args):
+        pass    
+
 def confirmPopup(title, msg, answerCallback):
     content = ConfirmPopup(text=msg)
     content.bind(on_answer=answerCallback)
@@ -95,7 +118,7 @@ def confirmPopup(title, msg, answerCallback):
                     auto_dismiss= False)
     popup.open()
     return popup
-    
+
 class ConfirmPopup(GridLayout):
     text = StringProperty()
     
