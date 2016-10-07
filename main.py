68de3d5dd8d520267f5d22f0837390a9645cb27f
Brent Picasso <brent@autosportlabs.com>
2016-10-07 13:14:50 -0700

    bump version

--- main.py
+++ main.py
@@ -20,7 +20,7 @@
 # have received a copy of the GNU General Public License along with
 # this code. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "1.7.6"
+__version__ = "1.7.7"
 import sys
 import os
 
