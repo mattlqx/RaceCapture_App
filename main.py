337a9ad801eb8ddd1d1a1ac462d96ed84d481d95
Brent Picasso <brent@autosportlabs.com>
2017-04-10 13:45:08 -0700

    bump for 1.9.1 release

--- main.py
+++ main.py
@@ -20,7 +20,7 @@
 # have received a copy of the GNU General Public License along with
 # this code. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "1.9.0"
+__version__ = "1.9.1"
 import sys
 import os
 
