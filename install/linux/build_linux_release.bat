f69428f7c6a9005fa481eae3630d661be0ad83e5
Brent Picasso <brent@autosportlabs.com>
2017-07-04 16:51:24 -0700

    add short hash to package filename if not on release version

--- install/linux/build_linux_release.bat
+++ install/linux/build_linux_release.bat
@@ -1,20 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 RELEASE_VERSION=${1}
 if [ $# -eq 0 ] ; then
         MAJOR=$(git describe | cut -d \- -f 1 | cut -d . -f 1)
         MINOR=$(git describe | cut -d \- -f 1 | cut -d . -f 2)
         BUGFIX=$(git describe | cut -d \- -f 1 | cut -d . -f 3)
-        VERSION=$(git describe --dirty --always)
-        RELEASE_VERSION=${MAJOR}.${MINOR}.${BUGFIX}
+        VERSION=$(git describe --exact-match HEAD 2>/dev/null)
+        HASH=$(git rev-parse --short HEAD)
+        
+        if [ ! -z "$VERSION" ]; then
+                RELEASE_VERSION=${VERSION}
+        else
+                RELEASE_VERSION=${MAJOR}.${MINOR}.${BUGFIX}_${HASH}
+        fi
 fi
-
 
 rm -rf dist build racecapture_linux_*.tgz
 
 pyinstaller --clean -y racecapture_linux.spec
 
 # pyinstaller includes unneeded files from share
+# improve the pyinstaller script and this can be removed
 rm -rf ./dist/racecapture/share
 
 #package into tar file
