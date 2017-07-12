b3a525d3561e0e46b88680ca827f78f603dd2e33
Brent Picasso <brent@autosportlabs.com>
2017-07-12 12:51:15 -0700

    refine to specify logfile

--- install/linux/startup/run_racecapture.sh
+++ install/linux/startup/run_racecapture.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-#first parameter is the watchdog flag, launch with -w 1 to enable watchdog
+# RaceCapture App launch script for Linux
+
+# launch with -w 1 to enable watchdog
+# launch with -l <logfile> to specify the logfile path
+
+
+LOGFILE=~/racecapture.log
 WATCHDOG=0
 
-while getopts ":w:" opt; do
+while getopts ":w:l:" opt; do
     case ${opt} in
     w)  WATCHDOG=$OPTARG
+        ;;
+    l)  LOGFILE=$OPTARG
         ;;
     esac
 done
 
-#create the configuration directories as needed
+# create the configuration directories as needed
 mkdir -p ~/.kivy
 mkdir -p ~/.config/racecapture
 
-
-#check for ar1100 resistive touch screen controller
+# check for ar1100 resistive touch screen controller
 if lsusb | grep -q 04d8:0c02
 then
   echo 'Configuring for  ar1100 resistive touch controller'
   cp -n ar1100_kivy_config.ini ~/.kivy/config.ini
 fi
 
-#check for ... capacitive touch screen controller
+# check for ft5406 capacitive touch screen controller
 if lsmod | grep -q rpi_ft5406
 then
   echo 'Configuring for ft5406 capacitive touch controller'
@@ -33,7 +40,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 while
-  ./racecapture
+  ./racecapture > $LOGFILE 2>&1
   if [[ $WATCHDOG -ne 1 ]]; then
     break
   fi
