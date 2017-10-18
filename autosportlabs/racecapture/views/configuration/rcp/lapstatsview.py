13716e46113fef3bf22266fe78e6cb37ace203d2
Brent Picasso <brent@autosportlabs.com>
2017-10-18 10:34:20 -0700

    guard around spurious changes with view_loaded flag.  resolves (hopefully) #1313

--- autosportlabs/racecapture/views/configuration/rcp/lapstatsview.py
+++ autosportlabs/racecapture/views/configuration/rcp/lapstatsview.py
@@ -39,13 +39,17 @@ class LapStatsView(BaseConfigView):
 
     def __init__(self, **kwargs):
         super(LapStatsView, self).__init__(**kwargs)
+        self.view_loaded = False
         self.register_event_type('on_config_updated')
         self.ids.lapstats.setControl(SettingsSwitch())
         self.ids.lapstats.bind(on_setting=self.on_lapstats_enabled)
         self.ids.predtime.setControl(SettingsSwitch())
         self.ids.predtime.bind(on_setting=self.on_predtime_enabled)
-        
+
     def on_lapstats_enabled(self, instance, value):
+        if not self.view_loaded:
+            return
+
         if self.lap_config:
             lap_cfg = self.lap_config
             self._normalize_lap_config(lap_cfg, self.gps_config, value)
@@ -53,33 +57,38 @@ class LapStatsView(BaseConfigView):
             self.dispatch('on_modified')
             if not value:
                 self.ids.predtime.setValue(False)
-        
+
     def on_predtime_enabled(self, instance, value):
+        if not self.view_loaded:
+            return
+
         if self.lap_config:
-            if value: #force lapstats enabled if we enable prdictive timing
+            if value:  # force lapstats enabled if we enable prdictive timing
                 self.ids.lapstats.setValue(True)
             rate = LapConfig.DEFAULT_PREDICTED_TIME_SAMPLE_RATE if value else 0
             config = self.lap_config
             config.predTime.sampleRate = rate
             config.stale = True
             self.dispatch('on_modified')
-    
+
     def _normalize_lap_config(self, lap_cfg, gps_cfg, lapstats_enabled):
         gps_sample_rate = gps_cfg.sampleRate
         rate = gps_sample_rate if lapstats_enabled else 0
         lap_cfg.set_primary_stats(rate)
-            
+
     def on_config_updated(self, rcp_cfg):
+        self.view_loaded = False
         lap_config = rcp_cfg.lapConfig
         gps_config = rcp_cfg.gpsConfig
-        
+
         primary_stats_enabled = lap_config.primary_stats_enabled()
         self._normalize_lap_config(lap_config, gps_config, primary_stats_enabled)
         self.ids.lapstats.setValue(primary_stats_enabled)
-        
+
         if lap_config.predTime.sampleRate > 0:
             self.ids.predtime.setValue(True)
-            
+
         self.gps_config = rcp_cfg.gpsConfig
         self.lap_config = lap_config
-                 
+        self.view_loaded = True
+
