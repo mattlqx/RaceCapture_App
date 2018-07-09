99082fbe20062ae59ef4bd0caccef19f724ba24f
Brent Picasso <brent@autosportlabs.com>
2018-07-09 12:02:30 -0700

    extend dynamic range of smoothing. resolves #1571

--- autosportlabs/racecapture/views/configuration/rcp/analogchannelsview.py
+++ autosportlabs/racecapture/views/configuration/rcp/analogchannelsview.py
@@ -62,6 +62,11 @@ class AnalogChannelsView(BaseMultiChannelConfigView):
         return rcp_cfg.analogConfig
 
 class AnalogChannel(BaseChannelView):
+    MIN_SMOOTHING_RANGE = 0
+    MAX_SMOOTHING_RANGE = 1000000
+    ALPHA_SMOOTHING_FACTOR = 0.0000001
+    DISPLAY_SCALING = 0.01
+
     Builder.load_string("""
 <AnalogChannel>:
     orientation: 'horizontal'
@@ -125,8 +130,8 @@ class AnalogChannel(BaseChannelView):
                         halign: 'right'
                     Slider:
                         id: smoothing
-                        min: 0
-                        max: 100
+                        min: root.MIN_SMOOTHING_RANGE
+                        max: root.MAX_SMOOTHING_RANGE
                         on_value: root.on_smoothing(*args)
                     FieldLabel:
                         text: 'More'
@@ -220,15 +225,21 @@ class AnalogChannel(BaseChannelView):
         self.channelConfig = None
 
     def on_smoothing(self, instance, value):
-        self.ids.smoothing_value.text = 'Max' if value > 99 else str(int(value))
-        try:
-            if self.channelConfig:
-                alpha = max(0.005, (100.0 - float(value)) * .01)
-                self.channelConfig.alpha = alpha
-                self.channelConfig.stale = True
-                self.dispatch('on_modified', self.channelConfig)
-        except:
-            pass
+        label = ''
+        if value == AnalogChannel.MIN_SMOOTHING_RANGE:
+            label = 'Min'
+        elif value == AnalogChannel.MAX_SMOOTHING_RANGE:
+            label = 'Max'
+        else:
+            label = str(int(value / (AnalogChannel.MAX_SMOOTHING_RANGE * AnalogChannel.DISPLAY_SCALING)))
+
+        self.ids.smoothing_value.text = label
+        if self.channelConfig:
+            alpha = 1.0 if value == 0 else (AnalogChannel.MAX_SMOOTHING_RANGE - float(value)) * AnalogChannel.ALPHA_SMOOTHING_FACTOR
+
+            self.channelConfig.alpha = alpha
+            self.channelConfig.stale = True
+            self.dispatch('on_modified', self.channelConfig)
 
     def on_linear_map_offset(self, instance, value):
         try:
@@ -295,7 +306,7 @@ class AnalogChannel(BaseChannelView):
             check_linear.active = False
             check_mapped.active = True
 
-        self.ids.smoothing.value = round(100 - (channelConfig.alpha * 100.0))
+        self.ids.smoothing.value = AnalogChannel.MAX_SMOOTHING_RANGE - (channelConfig.alpha / AnalogChannel.ALPHA_SMOOTHING_FACTOR)
         self.ids.linearscaling.text = str(channelConfig.linearScaling)
         self.ids.linearoffset.text = str(channelConfig.linearOffset)
         map_editor = self.ids.map_editor
